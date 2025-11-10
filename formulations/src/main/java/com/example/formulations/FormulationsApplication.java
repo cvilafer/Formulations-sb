2da3d079ed83f5bcfb83cf0a6085ed3a2cb382de
Carles Vila <cvilafer@gmail.com>
2025-11-10 20:13:22 +0100

    primer commit con la aplicacion spring boot de inicio

---
+++ formulations/src/main/java/com/example/formulations/FormulationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.formulations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulationsApplication.class, args);
+	}
+
+}
---
+++ formulations/src/test/java/com/example/formulations/FormulationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formulations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
